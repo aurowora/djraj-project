c6a2e520987125ccbea7d546cedaccd41090deb3
Aurora McGinnis <aurora@aurora.vg>
2024-03-19 17:36:09 -0500

    missing parameter to run_in_executor

--- forums/blocking.py
+++ forums/blocking.py
@@ -3,4 +3,4 @@ import functools
 
 
 async def spawn_blocking(func, *args, **kwargs):
-    return await asyncio.get_running_loop().run_in_executor(functools.partial(func, *args, **kwargs))
+    return await asyncio.get_running_loop().run_in_executor(None, functools.partial(func, *args, **kwargs))
