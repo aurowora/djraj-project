0c34cb1535da315644509f48070ec1516e387b04
Aurora McGinnis <aurora@aurora.vg>
2024-03-19 09:42:21 -0500

    implement topic and post flags (untested)

--- forums/db/posts.py
+++ forums/db/posts.py
@@ -6,36 +6,41 @@ from forums.db import mysql_date_to_python
 from pydantic import BaseModel
 
 
+# Post flags
+POST_IS_HIDDEN = 1 << 0
+
+
 class Post(BaseModel):
     post_id: Optional[int]
     topic_id: int
     author_id: int
     content: str
     created_at: datetime
+    flags: int
 
 
 def _maybe_row_to_post(row: dict) -> Optional[Post]:
     return Post(post_id=row["postID"], thread_id=row["threadID"], author_id=row["userID"], content=row["content"],
-                created_at=mysql_date_to_python(row["createdAt"]))
+                created_at=mysql_date_to_python(row["createdAt"]), flags=row["flags"]) if row is not None else None
 
 
 class PostRepository:
     def __init__(self, db: Pool):
         self.__db = db
 
-    async def get_post_by_id(self, post_id: int) -> Optional[Post]:
+    async def get_post_by_id(self, post_id: int, include_hidden=False) -> Optional[Post]:
         async with self.__db.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute(
-                    "SELECT postID, threadID, userID, content, createdAt FROM postsTable WHERE postID = %s;",
+                    "SELECT * FROM postsTable WHERE postID = %s;" if include_hidden else f"SELECT * FROM postsTable WHERE postID = %s AND (flags & {POST_IS_HIDDEN}) = 0;",
                     (post_id,))
                 return _maybe_row_to_post(await cur.fetchone())
 
-    async def get_posts_of_topic(self, topic_id: int, limit: int = 20, skip: int = 0) -> AsyncGenerator[Post, None]:
+    async def get_posts_of_topic(self, topic_id: int, limit: int = 20, skip: int = 0, include_hidden=False) -> AsyncGenerator[Post, None]:
         async with self.__db.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute(
-                    "SELECT postID, threadID, userID, content, createdAt FROM postsTable WHERE threadID = %s ORDER BY createdAt ASC LIMIT %s OFFSET %s;",
+                    "SELECT * FROM postsTable WHERE threadID = %s ORDER BY createdAt ASC LIMIT %s OFFSET %s;" if include_hidden else f"SELECT * FROM postsTable WHERE threadID = %s AND (flags & {POST_IS_HIDDEN}) = 0 ORDER BY createdAt ASC LIMIT %s OFFSET %s;",
                     (topic_id, limit, skip))
                 while row := cur.fetchone():
                     yield _maybe_row_to_post(row)
@@ -45,15 +50,15 @@ class PostRepository:
             async with conn.cursor() as cur:
                 if post.post_id is None:
                     # createdAt set by default func
-                    await cur.execute('INSERT INTO postsTable (threadID, userID, content) VALUES (%s, %s, %s);',
+                    await cur.execute('INSERT INTO postsTable (threadID, userID, content, flags) VALUES (%s, %s, %s, %s);',
                                       (post.topic_id, post.author_id, post.content))
                     post.post_id = cur.lastrowid
                     return post.post_id
                 else:
                     # createdAt deliberately excluded
                     num_rows = await cur.execute(
-                        'UPDATE threadsTable SET userID = %s, threadID = %s, content = %s WHERE threadID = %s;',
-                        (post.author_id, post.topic_id, post.content, post.post_id))
+                        'UPDATE threadsTable SET userID = %s, threadID = %s, content = %s, flags = %s WHERE threadID = %s;',
+                        (post.author_id, post.topic_id, post.content, post.flags, post.post_id))
                     if num_rows < 1:
                         raise KeyError(f'failed updating topic {post.post_id}: no such topic')
                     return post.post_id
--- forums/db/topics.py
+++ forums/db/topics.py
@@ -7,6 +7,11 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
+# Bitflags for Topic
+TOPIC_IS_HIDDEN = 1 << 0
+TOPIC_IS_PINNED = 1 << 1
+
+
 class Topic(BaseModel):
     """
     Topic represents a Topic in the database, which is a top level post created by some User (the author).
@@ -20,11 +25,12 @@ class Topic(BaseModel):
     title: str
     content: str
     created_at: Optional[datetime]
+    flags: int = 0
 
 
 def _maybe_row_to_topic(row: Optional[dict]) -> Optional[Topic]:
     return Topic(topic_id=row["threadID"], author_id=row["userID"], title=row["title"], content=row["content"],
-                 created_at=mysql_date_to_python(row["createdAt"])) if row is not None else None
+                 created_at=mysql_date_to_python(row["createdAt"]), flags=row["flags"]) if row is not None else None
 
 
 class TopicRepository:
@@ -34,18 +40,18 @@ class TopicRepository:
     def __init__(self, db: Pool):
         self.__db = db
 
-    async def get_topic_by_id(self, topic_id: int) -> Optional[Topic]:
+    async def get_topic_by_id(self, topic_id: int, include_hidden=False) -> Optional[Topic]:
         """
         Returns the topic with the given topic_id, or none if there is no such topic.
         """
         async with self.__db.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute(
-                    "SELECT threadID, userID, title, content, createdAt FROM threadsTable WHERE threadId = %s;",
+                    "SELECT * FROM threadsTable WHERE threadId = %s;" if include_hidden else f"SELECT * FROM threadsTable WHERE threadId = %s AND (flags & {TOPIC_IS_HIDDEN}) = 0;",
                     (topic_id,))
                 return _maybe_row_to_topic(await cur.fetchone())
 
-    async def get_topics_of_author(self, author_id: int, limit: int = 20, skip: int = 0) -> AsyncGenerator[Topic, None]:
+    async def get_topics_of_author(self, author_id: int, limit: int = 20, skip: int = 0, include_hidden=False) -> AsyncGenerator[Topic, None]:
         """
         Returns a generator over all topics from the given author, sorted by the creation time. This will return
         up to `limit` topics with an offset of `skip` from the beginning of the sorted topic set.
@@ -53,12 +59,12 @@ class TopicRepository:
         async with self.__db.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute(
-                    "SELECT threadID, userID, title, content, createdAt FROM threadsTable WHERE authorID = %s ORDER BY createdAt DESC LIMIT %s OFFSET %s;",
+                    "SELECT * FROM threadsTable WHERE authorID = %s ORDER BY createdAt DESC LIMIT %s OFFSET %s;" if include_hidden else f"SELECT * FROM threadsTable WHERE authorID = %s AND (flags & {TOPIC_IS_HIDDEN}) = 0 ORDER BY createdAt DESC LIMIT %s OFFSET %s;",
                     (author_id, limit, skip))
                 while row := await cur.fetchone():
                     yield _maybe_row_to_topic(row)  # is never None
 
-    async def search_topics(self, query: str, limit: int = 20, skip: int = 0) -> AsyncGenerator[Topic, None]:
+    async def search_topics(self, query: str, limit: int = 20, skip: int = 0, include_hidden=False) -> AsyncGenerator[Topic, None]:
         """
         Returns a generator over all topics that contain the phrase in the query, sorted by the creation time.
         This will return up to `limit` topics with an offset of `skip` from the beginning of the sorted topic set.
@@ -68,7 +74,7 @@ class TopicRepository:
         async with self.__db.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute(
-                    "SELECT threadID, userID, title, content, createdAt FROM threadsTable WHERE title LIKE %s ESCAPE '\\\\' OR content LIKE %s ESCAPE '\\\\' ORDER BY createdAt DESC LIMIT %s OFFSET %s;",
+                    "SELECT * FROM threadsTable WHERE title LIKE %s ESCAPE '\\\\' OR content LIKE %s ESCAPE '\\\\' ORDER BY createdAt DESC LIMIT %s OFFSET %s;" if include_hidden else f"SELECT * FROM threadsTable WHERE (title LIKE %s ESCAPE '\\\\' OR content LIKE %s ESCAPE '\\\\') AND (flags & {TOPIC_IS_HIDDEN}) = 0 ORDER BY createdAt DESC LIMIT %s OFFSET %s;",
                     (query, query, limit, skip)
                 )
                 while row := await cur.fetchone():
@@ -86,15 +92,15 @@ class TopicRepository:
             async with conn.cursor() as cur:
                 if topic.topic_id is None:
                     # createdAt set by default func
-                    await cur.execute('INSERT INTO threadsTable (userID, title, content) VALUES (%s, %s, %s);',
-                                      (topic.author_id, topic.title, topic.content))
+                    await cur.execute('INSERT INTO threadsTable (userID, title, content, flags) VALUES (%s, %s, %s, %s);',
+                                      (topic.author_id, topic.title, topic.content, topic.flags))
                     topic.topic_id = cur.lastrowid
                     return topic.topic_id
                 else:
                     # createdAt deliberately excluded
                     num_rows = await cur.execute(
-                        'UPDATE threadsTable SET userID = %s, title = %s, content = %s WHERE threadID = %s;',
-                        (topic.author_id, topic.title, topic.content, topic.topic_id))
+                        'UPDATE threadsTable SET userID = %s, title = %s, content = %s, flags = %s WHERE threadID = %s;',
+                        (topic.author_id, topic.title, topic.content, topic.flags, topic.topic_id))
                     if num_rows < 1:
                         raise KeyError(f'failed updating topic {topic.topic_id}: no such topic')
                     return topic.topic_id
