ae0b14a450089096a719c21186d26a16d5be65e3
Aurora McGinnis <aurora@aurora.vg>
2024-03-24 18:25:51 -0500

    capitialize and add period to error messages automatically

--- forums/routes/pages.py
+++ forums/routes/pages.py
@@ -6,6 +6,7 @@ from starlette.templating import Jinja2Templates
 from .auth import current_user, _assert_no_user, generate_csrf_token
 from ..db.users import User
 from forums.utils import get_templates
+import re
 
 pages_router = APIRouter()
 
@@ -15,12 +16,17 @@ async def index(user: User = Depends(current_user)) -> str:
     return f'Hello, {user.username}!'
 
 
+__ENDSWITH_PUNCT = re.compile('.*[.?!]$')
+
+
 @pages_router.get('/login', dependencies=[Depends(_assert_no_user)])
 def login(req: Request, error: Optional[str] = None, tpl: Jinja2Templates = Depends(get_templates), csrf_token: str = Depends(generate_csrf_token)):
     ctx = {'csrf_token': csrf_token}
 
     if error:
-        ctx["error"] = error
+        ctx["error"] = error.capitalize()
+        if __ENDSWITH_PUNCT.match(ctx["error"]) is None:
+            ctx["error"] = ctx["error"] + '.'
 
     return tpl.TemplateResponse(request=req, name='login.html', context=ctx)
 
@@ -30,7 +36,9 @@ def register(req: Request, error: Optional[str] = None, tpl: Jinja2Templates = D
     ctx = {'csrf_token': csrf_token}
 
     if error:
-        ctx["error"] = error
+        ctx["error"] = error.capitalize()
+        if __ENDSWITH_PUNCT.match(ctx["error"]) is None:
+            ctx["error"] = ctx["error"] + '.'
 
     print(ctx)
 
