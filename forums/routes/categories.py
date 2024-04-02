e68fc93ef08b5c9e8c38ed4e772c195766a31057
Aurora McGinnis <aurora@aurora.vg>
2024-04-02 13:13:41 -0500

    add category creation code

--- forums/routes/categories.py
+++ forums/routes/categories.py
@@ -1,17 +1,24 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Form, Request
 from starlette import status
+from starlette.responses import RedirectResponse
 
-from forums.db.categories import CategoryRepository
+from forums.db.categories import CategoryRepository, Category
 from forums.db.topics import TopicRepository
 from forums.db.users import User
 from forums.routes.auth import current_user
 from forums.routes.auth import csrf_verify
 from asyncio import gather
+from typing import Annotated
+import regex
 
 from forums.utils import get_category_repo, get_topic_repo, async_collect
 
 cat_router = APIRouter()
 TOPICS_PER_PAGE = 20
+
+_CATEGORY_ALLOWED_CHARS = regex.compile(r"$[\P{Cc}\P{Cn}\P{Cs}]+^")
+CATEGORY_NAME_MAX_SIZE = 64
+CATEGORY_DESC_MAX_SIZE = 128
 
 
 @cat_router.get('/{cat_id}')
@@ -38,12 +45,39 @@ async def category_index(cat_id: int, page: int, user: User = Depends(current_us
 
 
 @cat_router.post('/create')
-async def create_category():
-    raise NotImplemented
+async def create_category(
+        req: Request,
+        name: Annotated[str, Form()], desc: Annotated[str, Form()], parent: Annotated[int | None, Form()],
+        csrf_token: Annotated[str, Form()], user: User = Depends(current_user),
+        cat_repo: CategoryRepository = Depends(get_category_repo)):
+
+    # check privs
+    if not user.is_moderator():
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You do not have permission to do this.')
+
+    csrf_verify(req, csrf_token)
+
+    # check that the name / desc has valid chars and isnt too long
+    if not (0 < len(name) <= CATEGORY_NAME_MAX_SIZE) and _CATEGORY_ALLOWED_CHARS.match(name) is not None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Category name must be between 0 and {CATEGORY_NAME_MAX_SIZE} characters.')
+    if not (0 < len(desc) <= CATEGORY_DESC_MAX_SIZE) and _CATEGORY_ALLOWED_CHARS.match(desc) is not None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Category description must be between 0 '
+                                                                            f'and {CATEGORY_DESC_MAX_SIZE} characters')
+
+    # insert
+    new_cat = Category(cat_name=name, cat_desc=desc, parent_cat=parent, id=None)
+
+    # todo catch fk vio
+    await cat_repo.put_category(new_cat)
+
+    # Take them to the new category page
+    if new_cat.id is None:
+        raise TypeError
+    return RedirectResponse(status_code=status.HTTP_303_SEE_OTHER, url=f'/categories/{new_cat.id}')
 
 
 @cat_router.delete('/{cat_id}')
-async def delete_category(cat_id: int, user: User = Depends(current_user)):
+async def delete_category(cat_id: int, csrf_token: str, user: User = Depends(current_user)):
     raise NotImplemented
 
 
