b566be167229052ecc506f98d64bd86238a32973
Aurora McGinnis <aurora@aurora.vg>
2024-04-03 21:10:33 -0500

    edit posts

--- forums/routes/topic.py
+++ forums/routes/topic.py
@@ -6,7 +6,7 @@ from pymysql import IntegrityError
 from starlette import status
 from starlette.responses import RedirectResponse, Response
 
-from forums.db.posts import PostRepository, Post
+from forums.db.posts import PostRepository, Post, POST_IS_HIDDEN
 from forums.db.topics import TOPIC_ALL_FLAGS, Topic, TopicRepository, TOPIC_IS_HIDDEN, TOPIC_IS_PINNED
 from forums.db.users import User, IS_USER_RESTRICTED, IS_USER_MODERATOR, UserRepository, get_user_repo
 from forums.models import UserAPI
@@ -39,7 +39,8 @@ async def create_topic(req: Request,
         raise HTTPException(status_code=400, detail=f'Title must be between 1 and {MAX_TOPIC_TITLE_LEN} characters.')
 
     if not (0 < len(content) <= MAX_TOPIC_CONTENT_LEN):
-        raise HTTPException(status_code=400, detail=f'Post content must be between 1 and {MAX_TOPIC_CONTENT_LEN} characters.')
+        raise HTTPException(status_code=400,
+                            detail=f'Post content must be between 1 and {MAX_TOPIC_CONTENT_LEN} characters.')
 
     if category < 0 and create_flags < 0:
         raise HTTPException(status_code=400, detail='Category and create_flags must be positive integers.')
@@ -209,15 +210,18 @@ async def update_topic(req: Request, patch_spec: TopicPatchSpec, topic_id: int =
         try:
             await topic_repo.put_topic(topic)
         except IntegrityError:
-            raise HTTPException(status_code=400, detail='Cannot set category of topic because the target category is not valid.')
+            raise HTTPException(status_code=400,
+                                detail='Cannot set category of topic because the target category is not valid.')
 
     # TODO: figure out how to integrate this with the front end
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @topic_router.post('/{topic_id}/reply')
-async def reply_to_topic(req: Request, topic_id: int, content: Annotated[str, Form()], csrf_token: Annotated[str, Form()], user: User = Depends(current_user),
-                         topic_repo: TopicRepository = Depends(get_topic_repo), post_repo: PostRepository = Depends(get_post_repo)):
+async def reply_to_topic(req: Request, topic_id: int, content: Annotated[str, Form()],
+                         csrf_token: Annotated[str, Form()], user: User = Depends(current_user),
+                         topic_repo: TopicRepository = Depends(get_topic_repo),
+                         post_repo: PostRepository = Depends(get_post_repo)):
     if user.is_restricted():
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You do not have permission to do this.')
 
@@ -248,4 +252,61 @@ async def reply_to_topic(req: Request, topic_id: int, content: Annotated[str, Fo
 
     await post_repo.put_post(post)
 
-    return RedirectResponse(status_code=status.HTTP_303_SEE_OTHER, url=f'/{topic_id}/')
+    return RedirectResponse(status_code=status.HTTP_303_SEE_OTHER, url=f'/topic/{topic_id}/')
+
+
+class PostPatchSpec(BaseModel):
+    set_content: Optional[str]
+    set_hidden: Optional[bool]
+
+    csrf_token: str
+
+
+@topic_router.patch('/{topic_id}/{post_id}')
+async def edit_post(req: Request, topic_id: int, post_id: int, patch_spec: PostPatchSpec,
+                    user: User = Depends(current_user), topic_repo: TopicRepository = Depends(get_topic_repo),
+                    post_repo: PostRepository = Depends(get_post_repo)):
+    # restricted users cannot edit posts
+    if user.is_restricted():
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You do not have permission to do this.')
+
+    csrf_verify(req, patch_spec.csrf_token)
+
+    # load the topic and post
+    topic = await topic_repo.get_topic_by_id(topic_id)
+    post = await post_repo.get_post_by_id(post_id)
+    if not topic or not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Could not locate topic/post.')
+
+    # only the author and moderators may edit this
+    if post.author_id != user.user_id and not user.is_moderator():
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You do not have permission to do this.')
+    # if the topic or post is hidden, only a moderator can change it
+    if (
+            topic.flags & TOPIC_IS_HIDDEN == TOPIC_IS_HIDDEN or post.flags & POST_IS_HIDDEN == POST_IS_HIDDEN) and not user.is_moderator():
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You do not have permission to do this.')
+
+    dirty = False
+
+    if patch_spec.set_content is not None:
+        if __TOPIC_ALLOW_MOST_CHARS.match(patch_spec.set_content) is None or not (
+                0 < len(patch_spec.set_content) <= MAX_TOPIC_CONTENT_LEN):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='Post content is too long or contains illegal characters.')
+
+        post.content = patch_spec.set_content
+        dirty = True
+
+    if patch_spec.set_hidden is not None:
+        if patch_spec.set_hidden:
+            post.flags |= POST_IS_HIDDEN
+        elif user.is_moderator():
+            post.flags &= ~POST_IS_HIDDEN
+        else:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You do not have permission to do this.')
+        dirty = True
+
+    if dirty:
+        await post_repo.put_post(post)
+
+    return RedirectResponse(status_code=status.HTTP_303_SEE_OTHER, url=f'/topic/{topic_id}/')
