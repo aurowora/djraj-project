216b93639fe144975d3397e86ce3b47390d41858
Aurora McGinnis <aurora@aurora.vg>
2024-04-16 16:54:17 -0500

    update schema to allow for attachments

--- up.sql
+++ up.sql
@@ -4,6 +4,7 @@ CREATE TABLE `categories`
     `cat_name`   VARCHAR(64)      NOT NULL,
     `cat_desc`   VARCHAR(128)     NULL,
     `parent_cat` INTEGER UNSIGNED NULL,
+    `avatar`     varchar(128) null,
     CONSTRAINT `pk_categories_id` PRIMARY KEY (`id`),
     CONSTRAINT `fk_cat_parent_ref` FOREIGN KEY (`parent_cat`) REFERENCES `categories` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
@@ -43,6 +44,18 @@ CREATE TABLE `threadsTable`
 
 CREATE INDEX idx_created_at ON `threadsTable` (`createdAt`);
 
+CREATE TABLE `threadAttachments`
+(
+    `id` int unsigned not null auto_increment,
+    `thread` int unsigned not null,
+    `filename` varchar(128) not null,
+    `author` int unsigned not null,
+    `createdAt` timestamp not null default CURRENT_TIMESTAMP,
+    constraint `pk_thread_attachments_id` primary key (`id`),
+    constraint `fk_thread_attachments_thr` foreign key (`thread`) references `threadsTable` (`threadID`),
+    constraint `fk_thread_attachments_author` foreign key (`author`) references `loginTable` (`id`)
+);
+
 -- SHOW CREATE TABLE postsTable (mostly jerron)
 CREATE TABLE `postsTable`
 (
@@ -60,3 +73,15 @@ CREATE TABLE `postsTable`
   COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE INDEX idx_posts_table ON `postsTable` (`createdAt`);
+
+CREATE TABLE `postsAttachments`
+(
+    `id` int unsigned not null auto_increment,
+    `post` int unsigned not null,
+    `filename` varchar(128) not null,
+    `author` int unsigned not null,
+    `createdAt` timestamp not null default CURRENT_TIMESTAMP,
+    constraint `pk_posts_attachments_id` primary key (`id`),
+    constraint `fk_posts_attachments_post` foreign key (`post`) references `postsTable` (`postID`),
+    constraint `fk_posts_attachments_author` foreign key (`author`) references `loginTable` (`id`)
+);
